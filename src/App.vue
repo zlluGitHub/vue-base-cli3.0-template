2b73cb5a5c1c5ba7f17cb002545dee4bc768e2b5
zhenglinglu <l>
2020-06-24 22:58:28 +0800

    自动化部署测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="前端自动化部署平台（自动化测试）"/>
   </div>
 </template>
 
